a8c2115174a29df8c3a408241943288e1118e425
Gerard Roche <gerardroche@users.noreply.github.com>
2014-12-23 14:04:22 +0000

    Auto disable native PHP package
    Thanks https://github.com/SublimeText-Markdown/MarkdownEditing

---
+++ php-grammar.py
@@ -0,0 +1,15 @@
+import sublime
+
+def plugin_loaded():
+    # Native package causes some conflicts.
+    # Thanks https://github.com/SublimeText-Markdown/MarkdownEditing
+    disable_native_php_package()
+
+def disable_native_php_package():
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    ignored_packages = settings.get('ignored_packages', [])
+
+    if 'PHP' not in ignored_packages:
+        ignored_packages.append('PHP')
+        settings.set('ignored_packages', ignored_packages)
+        sublime.save_settings('Preferences.sublime-settings')
