0fcd55f81325c20617a1e7ac500239cac4e18f15
Gerard Roche <gerardroche@users.noreply.github.com>
2015-07-01 15:40:00 +0100

    Add test for inline inheritdoc phpdoc tags; Closes #13

---
+++ test/syntax/gt_or_eq_3092/syntax_test_issue_13_phpdoc_inline_keywords.php
@@ -0,0 +1,10 @@
+// SYNTAX TEST "Packages/php-grammar/PHP.tmLanguage"
+<?php
+
+/**
+ * {@inheritdoc}
+//      ^ meta.tag.inline.phpdoc keyword
+ *
+ * A phpdoc inline {@inheritdoc} keyword
+//                      ^ meta.tag.inline.phpdoc keyword
+ */
