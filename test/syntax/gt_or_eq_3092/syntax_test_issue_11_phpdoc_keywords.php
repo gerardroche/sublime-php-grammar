29f4efb8121edbeaca199ad86781c0b39e4f2b79
Gerard Roche <gerardroche@users.noreply.github.com>
2015-07-01 15:34:07 +0100

    Add test for issue #11; Closes #11

---
+++ test/syntax/gt_or_eq_3092/syntax_test_issue_11_phpdoc_keywords.php
@@ -0,0 +1,13 @@
+// SYNTAX TEST "Packages/php-grammar/PHP.tmLanguage"
+<?php
+
+/**
+ * @keyword
+//      ^ keyword
+ */
+
+/**
+ * (c) Some Guy <some_guy@gmail.com>
+//                  ^ -keyword
+//                          ^ -keyword
+ */
