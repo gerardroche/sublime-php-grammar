dee45aeebf4081df091d578ed3d52e74b1da93e7
Gerard Roche <gerardroche@users.noreply.github.com>
2015-07-02 18:16:58 +0100

    Add todo test for #17

---
+++ test/indentation/psr_2_example_4_4_b_method_arguments_todo.php
@@ -0,0 +1,40 @@
+--TEST--
+PSR 2 Coding Style Guide Example 4.4 (b)
+
+https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
+
+TODO see https://github.com/gerardroche/sublime-php-grammar/issues/17
+--FILE--
+<?php
+
+namespace Vendor\Package;
+
+class ClassName
+{
+    public function aVeryLongMethodName(
+        ClassTypeHint $arg1,
+        &$arg2,
+        array $arg3 = []
+    ) {
+        echo 'method body';
+    }
+}
+
+?>
+--EXPECT--
+<?php
+
+namespace Vendor\Package;
+
+class ClassName
+{
+    public function aVeryLongMethodName(
+        ClassTypeHint $arg1,
+        &$arg2,
+        array $arg3 = []
+    ) {
+        echo 'method body';
+    }
+}
+
+?>
