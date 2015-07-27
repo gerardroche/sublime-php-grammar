3ba8f6f24fe2bb6e13d33ffd27347d68f3afad9c
Gerard Roche <gerardroche@users.noreply.github.com>
2015-07-27 21:52:51 +0100

    syntax tests: add support class builtin tests

--- tests/syntax/gt_or_eq_3092/syntax_test_support_class_builtin.php
+++ tests/syntax/gt_or_eq_3092/syntax_test_support_class_builtin.php
@@ -22,3 +22,29 @@ class x extends \Exception {}
 //              ^ punctuation.separator.inheritance
 //                  ^ support.class.builtin
 
+class x implements Countable, Iterator {}
+//                      ^ support.class.builtin
+//                              ^ support.class.builtin
+
+class x implements \Countable, \Iterator {}
+//                 ^ punctuation.separator.inheritance
+//                      ^ support.class.builtin
+//                             ^ punctuation.separator.inheritance
+//                                  ^ support.class.builtin
+
+class x implements \Countable, Iterator {}
+//                 ^ punctuation.separator.inheritance
+//                      ^ support.class.builtin
+//                                  ^ support.class.builtin
+
+class x implements Countable, \Iterator {}
+//                      ^ support.class.builtin
+//                            ^ punctuation.separator.inheritance
+//                                  ^ support.class.builtin
+
+function x(Iterator $i) {}
+//          ^ support.class.builtin
+
+function x(\Iterator $i) {}
+//         ^ punctuation.separator.inheritance
+//              ^ support.class.builtin
