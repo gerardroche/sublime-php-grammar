cc3b5cbb92b5436a5c48b98e5e9a10521f6f2aca
Gerard Roche <gerardroche@users.noreply.github.com>
2016-01-13 00:09:41 +0000

    Add namespace with braces syntax test

---
+++ tests/syntax/gt_or_eq_3092/syntax_test_namespace_using_braces.php
@@ -0,0 +1,32 @@
+// SYNTAX TEST "Packages/php-grammar/PHP.tmLanguage"
+<?php
+
+// @todo https://github.com/gerardroche/sublime-php-grammar/issues/52
+
+namespace Xyz {
+// ^ meta.namespace
+//        ^ meta.namespace entity.name.type.namespace
+}
+
+namespace N\a\m\e {
+//  ^ meta.namespace
+//        ^ meta.namespace entity.name.type.namespace
+//          ^ meta.namespace entity.name.type.namespace
+//            ^ meta.namespace entity.name.type.namespace
+//              ^ meta.namespace entity.name.type.namespace
+}
+
+namespace Xyz
+// ^ meta.namespace
+//        ^ meta.namespace entity.name.type.namespace
+{}
+
+namespace N\a\m\e
+//  ^ meta.namespace
+//        ^ meta.namespace entity.name.type.namespace
+//          ^ meta.namespace entity.name.type.namespace
+//            ^ meta.namespace entity.name.type.namespace
+//              ^ meta.namespace entity.name.type.namespace
+{
+
+}
