11573eebb0cc5c711d461eb498d8a0d504cd6ea9
Gerard Roche <gerardroche@users.noreply.github.com>
2016-01-19 02:59:27 +0000

    Add type casting syntax tests

---
+++ tests/syntax/gt_or_eq_3092/syntax_test_type_cast.php
@@ -0,0 +1,68 @@
+// SYNTAX TEST "Packages/php-grammar/PHP.tmLanguage"
+<?php
+
+(bool);
+// ^ storage.type
+(boolean);
+// ^ storage.type
+
+(int);
+// ^ storage.type
+(integer);
+// ^ storage.type
+
+(float);
+// ^ storage.type
+(double);
+// ^ storage.type
+(real);
+// ^ storage.type
+
+(string);
+// ^ storage.type
+
+(object);
+// ^ storage.type
+
+(unset);
+// ^ storage.type
+
+(binary);
+// ^ storage.type
+
+(array) (int) (string);
+// ^ storage.type
+//        ^ storage.type
+//              ^ storage.type
+
+(array)(int)(string);
+// ^ storage.type
+//       ^ storage.type
+//              ^ storage.type
+
+$test = (int) $test;
+//        ^ storage.type
+
+$test = (int) (object)(string)(float) $test;
+//        ^ storage.type
+//              ^ storage.type
+//                      ^ storage.type
+//                              ^ storage.type
+
+$test = (int) $test;
+//        ^ storage.type
+
+$test = (float) (string) (int) $test;
+//        ^ storage.type
+
+call((int) $id);
+//     ^ storage.type
+
+call(
+    (int) $id,
+//    ^ storage.type
+    (array) $id,
+//    ^ storage.type
+    (string) $id
+//    ^ storage.type
+);
