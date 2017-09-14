fb256031dee667675824486847eceaf4b2b0f75a
Gerard Roche <gerardroche@users.noreply.github.com>
2017-09-14 09:54:15 +0100

    Reenable PHP when unloaded/uninstalled/disabled

--- plugin.py
+++ plugin.py
@@ -3,45 +3,51 @@ import sublime_plugin
 
 
 def plugin_loaded():
-    disable_native_php_package()
-
-
-def disable_native_php_package():
     settings = sublime.load_settings('Preferences.sublime-settings')
     ignored_packages = settings.get('ignored_packages', [])
 
+    # Disable the default PHP package
     if 'PHP' not in ignored_packages:
-
         # fixes #45 "error loading syntax" on install
         for window in sublime.windows():
             for view in window.views():
-                view_settings = view.settings()
-                syntax = view_settings.get('syntax')
-
-                if syntax.find('Packages/PHP/') != -1:
+                if view.settings().get('syntax').find('Packages/PHP/') != -1:
                     if int(sublime.version()) >= 3092:
-                        view_settings.set('syntax', 'Packages/php-grammar/PHP.sublime-syntax')
+                        view.settings().set('syntax', 'Packages/php-grammar/PHP.sublime-syntax')
                     else:
-                        view_settings.set('syntax', 'Packages/php-grammar/PHP.tmLanguage')
+                        view.settings().set('syntax', 'Packages/php-grammar/PHP.tmLanguage')
 
-                if syntax.find('Packages/HTML/') != -1:
+                if view.settings().get('syntax').find('Packages/HTML/') != -1:
                     file_name = view.file_name()
                     if file_name:
                         if file_name.endswith('.phtml'):
                             if int(sublime.version()) >= 3092:
-                                view_settings.set('syntax', 'Packages/php-grammar/PHP.sublime-syntax')
+                                view.settings().set('syntax', 'Packages/php-grammar/PHP.sublime-syntax')
                             else:
-                                view_settings.set('syntax', 'Packages/php-grammar/PHP.tmLanguage')
+                                view.settings().set('syntax', 'Packages/php-grammar/PHP.tmLanguage')
                         else:
+                            # In builds < 3092 setting the HTML syntax causes "error loading PHP syntax" errors
                             if int(sublime.version()) >= 3092:
-                                view_settings.set('syntax', 'Packages/HTML/HTML.sublime-syntax')
+                                view.settings().set('syntax', 'Packages/HTML/HTML.sublime-syntax')
                             else:
-                                # Setting the HTML syntax causes "error loading PHP syntax" errors
-                                # in builds less than 3092.
-                                view_settings.set('syntax', 'Packages/Text/Plain text.tmLanguage')
+                                view.settings().set('syntax', 'Packages/Text/Plain text.tmLanguage')
 
         ignored_packages.append('PHP')
         ignored_packages.sort()
+
+        settings.set('ignored_packages', ignored_packages)
+        sublime.save_settings('Preferences.sublime-settings')
+
+
+def plugin_unloaded():
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    ignored_packages = settings.get('ignored_packages', [])
+
+    # Reenable the default PHP package
+    if 'PHP' in ignored_packages:
+        ignored_packages.remove('PHP')
+        ignored_packages.sort()
+
         settings.set('ignored_packages', ignored_packages)
         sublime.save_settings('Preferences.sublime-settings')
 
@@ -50,6 +56,7 @@ def is_finish_completion_context(view):
     for s in view.sel():
         if view.substr(s.end()) != ')':
             return False
+
     return True
 
 
